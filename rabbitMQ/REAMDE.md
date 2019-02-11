cd3cfe3e2f70707f65aedcea4e5201a5001ee2c2
Becky Reamy <rlreamy@umich.edu>
2019-02-11 13:35:02 -0500

    KPMP-694: Initial Hello world message to queue

---
+++ rabbitMQ/REAMDE.md
@@ -0,0 +1,9 @@
+# RabbitMQ
+You will need to download and install rabbitMQ on your machine:
+https://www.rabbitmq.com/download.html
+
+To start it as a service, type (on a mac): 
+- export PATH=$PATH:/usr/local/sbin
+- rabbitmq-server
+
+
---
+++ rabbitMQ/src/main/java/org/kpmp/rabbitmq/rabbitMQ/DemoApplication.java
@@ -0,0 +1,21 @@
+package org.kpmp.rabbitmq.rabbitMQ;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	private final static String QUEUE_NAME = "hello";
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Autowired
+	public DemoApplication(Sender sender, Receiver receiver) throws Exception {
+		sender.send(QUEUE_NAME);
+		receiver.receive(QUEUE_NAME);
+	}
+}
