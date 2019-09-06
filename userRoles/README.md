365ab3af3198247afb9e172d37300d8fbab051d4
Becky Reamy <rlreamy@umich.edu>
2019-09-06 12:34:43 -0400

    KPMP-971: Adding what I have done

--- userRoles/README.md
+++ userRoles/README.md
@@ -9,3 +9,10 @@ What we need to do:
 2) The X-API-TOKEN header needs to be populated with the token from above for any request to the api
 
 3) If you make a request for a user that does not exist, it will throw an exception, so we will need to handle that gracefully
+
+4) We will have to define a group for each application (or one that can be used for all?) and make sure users are assigned to those groups
+
+5) Querying the user portal will return a JSON object like this:
+{"_id":"5cc9e10a2cda37d4db7d9b2d","active":true,"email":"jcarson@uw.edu","first_name":"Jonas","groups":["uw_rit_kpmp_role_developer","uw_rit_kpmp_app_userportal"],"last_name":"Carson","organization_id":"5cc9e10a2cda37d4db7d9b19","phone_numbers":null,"shib_id":"jcarson@washington.edu"}
+
+6) We will use values from the 'groups' section to determine if a user has permission.  We should also interrogate the 'active' property to ensure it is set to true.
--- userRoles/src/main/java/userRolesService/UserRoleController.java
+++ userRoles/src/main/java/userRolesService/UserRoleController.java
@@ -1,7 +1,12 @@
 package userRolesService;
 
+import java.io.UnsupportedEncodingException;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -16,10 +21,16 @@ import org.springframework.web.client.RestTemplate;
 @Controller
 public class UserRoleController {
 
+	private UTF8Encoder encoder;
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-	@RequestMapping(value = "/v1/user/roles/{shibId}", method = RequestMethod.GET)
-	public @ResponseBody String getUserRoles(@PathVariable("shibId") String shibId) {
+	@Autowired
+	public UserRoleController(UTF8Encoder encoder) {
+		this.encoder = encoder;
+	}
+
+	@RequestMapping(value = "/v1/user/info/{shibId}", method = RequestMethod.GET)
+	public @ResponseBody String getUserInfo(@PathVariable("shibId") String shibId) {
 
 		HttpHeaders headers = new HttpHeaders();
 		// We will need to create a token for each application
@@ -40,6 +51,43 @@ public class UserRoleController {
 			log.error(e.getMessage());
 			return null;
 		}
+	}
 
+	@RequestMapping(value = "/v1/login", method = RequestMethod.POST)
+	public @ResponseBody boolean login(HttpServletRequest request) throws UnsupportedEncodingException {
+		String value = handleNull(request.getHeader("eppn"));
+		String shibId = encoder.convertFromLatin1(value);
+
+		HttpHeaders headers = new HttpHeaders();
+		// We will need to create a token for each application
+		headers.set("X-API-TOKEN", "1ef4ecb8-2c2a-48af-889b-3ceb9ca9d102");
+		HttpEntity<String> entity = new HttpEntity<>("body", headers);
+
+		RestTemplate restTemplate = new RestTemplate();
+		try {
+			ResponseEntity<String> result = restTemplate.exchange("https://testuserportal.kpmp.org/api/user/" + shibId,
+					HttpMethod.GET, entity, String.class);
+			log.info(result.getStatusCode().toString());
+			String resultString = result.getBody();
+
+			log.info(resultString);
+
+		} catch (Exception e) {
+			log.error(e.getMessage());
+		}
+
+		return true;
+	}
+
+	@RequestMapping(value = "/v1/secured", method = RequestMethod.GET)
+	public @ResponseBody String getValue() {
+		return "You can see me";
+	}
+
+	private String handleNull(String value) {
+		if (value == null) {
+			return "";
+		}
+		return value;
 	}
 }
