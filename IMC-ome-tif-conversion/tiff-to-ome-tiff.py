8b358adb5466700ad37de335be08f4900a6c4082
dert1129 <nathancreger1111@gmail.com>
2024-02-01 11:08:14 -0500

    Add logic for codex and IMC images

--- IMC-ome-tif-conversion/tiff-to-ome-tiff.py
+++ IMC-ome-tif-conversion/tiff-to-ome-tiff.py
@@ -2,50 +2,44 @@ from tifffile import TiffWriter, imread
 import pandas as pd
 import os
 import sys
-import argparse
 from os.path import join
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-  "-d",
-  "--dir",
-  required=True,
-  help="The directory where the .tiff and .txt file you want to convert are located"
-)
-args = parser.parse_args()
-if os.path.isdir(args.dir):
+directory = "/home/zwright/Documents/KPMP/CODEX_2024/data"
+if os.path.isdir(directory):
 
   channel_names = None
   img_path = None
-  directory = args.dir
   for filename in os.scandir(directory):
     extension = os.path.splitext(filename)[1]
-    if extension == ".tiff":
+    if extension == ".tif" or extension == ".tiff":
       img_path = join("raw", filename.path)
-      output_path = join("processed", os.path.splitext(filename)[0] + "-ome" + extension)
-    elif extension == ".txt":
+      output_path = join("processed", os.path.splitext(filename)[0] + ".ome" + extension)
+    elif extension == ".csv" or extension == ".txt":
       channel_names_path = join("raw", filename.path)
-      df = pd.read_csv(channel_names_path, sep="\t")
-      channel_names = df['Label'].tolist()
+      df = pd.read_csv(channel_names_path, sep=",")
+      if 'Marker' in df:
+          channel_names = df['Marker'].tolist()
+      elif 'Label' in df:
+          channel_names = df['Label'].tolist()
     else:
       print("Only .tiff and .txt files are supported.")
       sys.exit()
+
+    if (img_path and channel_names_path):  
+        img_arr = imread(img_path)
+        img_arr.shape
+        img_arr.dtype
+        
+        tiff_writer = TiffWriter(output_path, ome=True)
+        tiff_writer.write(
+          img_arr,
+          metadata={
+              'axes': "CYX",
+              'Channel': {'Name': channel_names},
+          }
+        )
+        tiff_writer.close()
     
-    if img_path and channel_names:
-      img_arr = imread(img_path)
-      img_arr.shape
-      img_arr.dtype
-
-      tiff_writer = TiffWriter(output_path, ome=True)
-      tiff_writer.write(
-        img_arr,
-        metadata={
-            'axes': "CYX",
-            'Channel': {'Name': channel_names},
-        }
-      )
-      tiff_writer.close()
-
 else:
   print('Directory is not valid')
   sys.exit()
